e8cea5021d4473bbb19ed564a015c9d11813eae2
Rails Applications <ukab23@gmail.com>
2017-11-02 11:37:02 +0000

    Creting new articles and editing deleting them

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,60 @@
+class ArticlesController < ApplicationController
+	
+	before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+	def index
+		@articles = Article.all
+	end	
+
+
+	def new
+		@article = Article.new
+	end
+
+
+	def edit
+	end
+
+
+	def create
+		@article = Article.new(article_params)
+    	if @article.save
+    		flash[:notice] = "Thank You for your wishes..!"
+    		redirect_to article_path(@article)
+    	else
+    		render :new	
+    	end	
+
+	end
+
+
+
+	def update
+		if @article.update(article_params)
+    		flash[:notice] = "Thank You for your wishes..!"
+    		redirect_to article_path(@article)
+    	else
+    		render :edit	
+    	end	
+	end
+
+
+	def show
+	end
+
+	def destroy
+		@article.destroy
+		flash[:notice] = "Deleted Successfuly.."
+
+	end	
+
+	private
+
+        def set_article
+        	@article = Article.find(params[:id])
+        end	
+
+		def article_params
+			params.require(:article).permit(:name, :wish)
+		end	
+end
