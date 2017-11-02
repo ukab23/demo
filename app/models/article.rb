043b857a93e0803e008a58d61c846347fd5ea549
Rails Applications <ukab23@gmail.com>
2017-11-02 11:06:47 +0000

    Creating articles with validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ActiveRecord::Base
-    
+    validates :name, presence: true, length: {minimum: 3, maximum: 25}
+    validates :wish, presence: true, length: {minimum: 5, maximum: 200}
 end
